82538a1368759ddcd6a0e6abfe27c2ef1b477764
leonardpaulus <leonard.paulus@agenturignis.de>
2022-02-04 08:05:56 +0100

    ✨ Add page content

--- app.js
+++ app.js
@@ -2,5 +2,24 @@ var app = new Vue({
   el: "#events",
   data: {
     counter: 0,
+    name: "",
+    confirmedName: "",
+  },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+    submitForm() {
+      alert("Form has been submitted");
+    },
+    confirmName() {
+      this.confirmedName = this.name;
+    },
   },
 });
