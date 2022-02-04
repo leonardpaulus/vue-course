fb5dc58da87f5fda345fa9d8e825e626ca8fdd92
leonardpaulus <leonard.paulus@agenturignis.de>
2022-02-04 11:16:12 +0100

    ✨ Add dynamic styling

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 var app = new Vue({
-  el: "#user-goal",
-  data: {
-    courseGoal: "Finish the course and learn Vue! :)",
+  el: "#styling",
+  data() {
+    return {
+      boxAselected: false,
+      boxBselected: false,
+      boxCselected: false,
+    };
+  },
+  methods: {
+    boxSelected(box) {
+      if (box === "A") {
+        this.boxAselected = !this.boxAselected;
+      } else if (box === "B") {
+        this.boxBselected = !this.boxBselected;
+      } else if (box === "C") {
+        this.boxCselected = !this.boxCselected;
+      }
+    },
+  },
+  computed: {
+    boxAclasses() {
+      return { active: this.boxAselected };
+    },
   },
 });
